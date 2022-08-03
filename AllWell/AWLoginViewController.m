ee409b5ea2423af053333f415e003f0b3746ae71
zhoushaolin <zhoushaolin@kuaishou.com>
2022-08-03 16:44:41 +0800

    更新UI

--- AllWell/AWLoginViewController.m
+++ AllWell/AWLoginViewController.m
@@ -6,16 +6,38 @@
 //
 
 #import "AWLoginViewController.h"
+#import "UIColor+RGBA.h"
 
 @interface AWLoginViewController ()
-
+@property (nonatomic, weak)IBOutlet UIView *loginView;
+@property (nonatomic, weak)IBOutlet UIView *registerView;
+@property (nonatomic, weak)IBOutlet UIButton *switchButton;
 @end
 
 @implementation AWLoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithRGB:0xF6F6F6];
+    self.loginView.layer.cornerRadius = 6.0;
+    self.registerView.layer.cornerRadius = 6.0;
+    self.switchButton.layer.cornerRadius = 6.0;
+    self.registerView.hidden = YES;
+    [self.switchButton setTitle:@"Register" forState:UIControlStateNormal];
+    [self.switchButton setTitle:@"Login" forState:UIControlStateSelected];
+    [self.switchButton setBackgroundColor:[UIColor colorWithRGB:0xFFFFFF]];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)switchAction:(UIButton *)sender {
+    sender.selected = !sender.selected;
+    if (sender.selected) {
+        self.registerView.hidden = NO;
+        self.loginView.hidden = YES;
+    } else {
+        self.registerView.hidden = YES;
+        self.loginView.hidden = NO;
+    }
 }
 
 /*
--- AllWell/AWMainViewController.m
+++ AllWell/AWMainViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "AWMainViewController.h"
+#import "AWDataHelper.h"
+#import "AWLoginViewController.h"
 
 @interface AWMainViewController ()
 
@@ -15,6 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if ([AWDataHelper shared].user == nil) {
+        AWLoginViewController *loginController = [self.storyboard instantiateViewControllerWithIdentifier:@"login"];
+        loginController.modalPresentationStyle = UIModalPresentationFullScreen;
+        [self.navigationController presentViewController:loginController animated:NO completion:nil];
+    }
     // Do any additional setup after loading the view.
 }
 
